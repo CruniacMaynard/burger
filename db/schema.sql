81ed7121da0fa03bdd635ff66e52ec572d636046
Cruniac <s.maynard21@yahoo.com>
2018-02-08 17:46:07 -0500

    hw files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(400) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
